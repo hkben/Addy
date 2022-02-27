eb3f07ce15f9e7afbc3f99e6ddaac92279459a24
hkben <liu411hk@gmail.com>
2022-02-28 06:30:11 +0800

    Use shadow Dom to separate css from host website

--- src/pages/Content/index.tsx
+++ src/pages/Content/index.tsx
@@ -4,7 +4,7 @@ import Browser from 'webextension-polyfill';
 import { IBrowserMessage } from '../interface';
 import Content from './Content';
 
-import './index.scss';
+import css from '!!css-loader!sass-loader!./index.scss';
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
@@ -29,14 +29,22 @@ let toggleSelectionTool = () => {
     return;
   }
 
-  let div: HTMLDivElement;
-  div = document.createElement('div');
-  div.id = 'webextension_content';
-  document.body.appendChild(div);
+  let shadowHost = document.createElement('section');
+  document.body.appendChild(shadowHost);
+
+  let shadowRoot = shadowHost.attachShadow({ mode: 'closed' });
+
+  let style = document.createElement('style');
+  style.innerHTML = css.toString();
+  shadowRoot.appendChild(style);
+
+  let contentDiv = document.createElement('div');
+  contentDiv.id = 'webextension_content';
+  shadowRoot.appendChild(contentDiv);
 
   var reactElement = React.createElement(Content);
 
-  render(reactElement, window.document.querySelector('#webextension_content'));
+  render(reactElement, contentDiv);
 
   init = true;
 };
