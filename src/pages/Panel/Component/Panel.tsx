e21136e66752b86e18ad7eb906a70c4faae3634c
hkben <liu411hk@gmail.com>
2022-03-03 00:50:39 +0800

    Move Panel to React Hook

--- src/pages/Panel/Component/Panel.tsx
+++ src/pages/Panel/Component/Panel.tsx
@@ -6,56 +6,54 @@ import Home from './Home';
 import Import from './Import';
 import Settings from './Settings';
 
-class Panel extends React.Component<{}> {
+function Panel() {
   // constructor(props: {}) {
   //   super(props);
 
   //   // this.newCollection = this.newCollection.bind(this);
   // }
 
-  render() {
-    return (
-      <HashRouter>
-        <div>
-          <nav className="flex items-center justify-between max-w-3xl p-4 mx-auto">
-            <a
-              className="inline-flex items-center justify-center w-10 h-10 bg-gray-100 rounded-lg dark:bg-gray-700"
-              href="/"
-            >
-              <img src="icon-34.png" />
-            </a>
+  return (
+    <HashRouter>
+      <div>
+        <nav className="flex items-center justify-between max-w-3xl p-4 mx-auto">
+          <a
+            className="inline-flex items-center justify-center w-10 h-10 bg-gray-100 rounded-lg dark:bg-gray-700"
+            href="/"
+          >
+            <img src="icon-34.png" />
+          </a>
 
-            <ul className="flex items-center space-x-2 text-sm font-medium text-gray-500 dark:text-gray-50">
-              <li>
-                <Link to="/" className="px-3 py-2 rounded-lg cursor-pointer">
-                  Home
-                </Link>
-              </li>
+          <ul className="flex items-center space-x-2 text-sm font-medium text-gray-500 dark:text-gray-50">
+            <li>
+              <Link to="/" className="px-3 py-2 rounded-lg cursor-pointer">
+                Home
+              </Link>
+            </li>
 
-              <li>
-                <Link
-                  to="/setting"
-                  className="px-3 py-2 rounded-lg cursor-pointer"
-                >
-                  Settings
-                </Link>
-              </li>
-            </ul>
-          </nav>
+            <li>
+              <Link
+                to="/setting"
+                className="px-3 py-2 rounded-lg cursor-pointer"
+              >
+                Settings
+              </Link>
+            </li>
+          </ul>
+        </nav>
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="setting" element={<Settings />}>
-              <Route index element={<Export />} />
-              <Route path="general" element={<General />} />
-              <Route path="export" element={<Export />} />
-              <Route path="import" element={<Import />} />
-            </Route>
-          </Routes>
-        </div>
-      </HashRouter>
-    );
-  }
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="setting" element={<Settings />}>
+            <Route index element={<Export />} />
+            <Route path="general" element={<General />} />
+            <Route path="export" element={<Export />} />
+            <Route path="import" element={<Import />} />
+          </Route>
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
 
 export default Panel;
