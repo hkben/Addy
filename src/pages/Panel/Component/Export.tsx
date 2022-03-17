48d3755bdf6e38d76781e3714a360095715f6f48
hkben <liu411hk@gmail.com>
2022-03-18 06:09:31 +0800

    Move Export to React Hook

--- src/pages/Panel/Component/Export.tsx
+++ src/pages/Panel/Component/Export.tsx
@@ -1,40 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Storage from '../../storage';
 
 interface State {
   json: string;
 }
 
-class Export extends React.Component<{}, State> {
-  state = {
-    json: '',
-  };
+function Export() {
+  const [json, setJson] = React.useState('');
 
-  async componentDidMount() {
-    console.log('componentDidMount');
-    await this.updateCollections();
-  }
+  useEffect(() => {
+    updateCollections().catch(console.error);
+  }, []);
 
-  async updateCollections() {
+  const updateCollections = async () => {
     let storage = await Storage.getStorage();
     let _json = JSON.stringify(storage);
-    this.setState({ json: _json });
-  }
+    setJson(_json);
+  };
 
-  render() {
-    return (
-      <div>
-        <p className="text-3xl py-2">Export</p>
-        <div className="w-full py-2">
-          <textarea
-            className="w-full h-64 p-2 border border-gray-500 rounded-md dark:bg-gray-800"
-            value={this.state.json}
-            readOnly
-          />
-        </div>
+  return (
+    <div>
+      <p className="text-3xl py-2">Export</p>
+      <div className="w-full py-2">
+        <textarea
+          className="w-full h-64 p-2 border border-gray-500 rounded-md dark:bg-gray-800"
+          value={json}
+          readOnly
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Export;
