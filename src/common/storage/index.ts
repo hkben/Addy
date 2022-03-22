374387c189f9147c928659fefb70854eb3eca20a
hkben <liu411hk@gmail.com>
2022-03-22 20:54:14 +0800

    Create Storage index.ts

---
+++ src/common/storage/index.ts
@@ -0,0 +1,7 @@
+import Storage from './storage';
+import Collections from './collections';
+import Collection from './collection';
+import CollectionItem from './collectionItem';
+import Setting from './setting';
+
+export { Storage, Collections, Collection, CollectionItem, Setting };
