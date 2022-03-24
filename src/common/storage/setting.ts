211485b04f907b4935b39e26b71dbc175934bea8
hkben <liu411hk@gmail.com>
2022-03-24 17:54:22 +0800

    Update storage setting method

--- src/common/storage/setting.ts
+++ src/common/storage/setting.ts
@@ -1,33 +1,82 @@
 import _ from 'lodash';
 import Browser from 'webextension-polyfill';
-import { ISetting } from '../interface';
-import Storage from './storage';
+import { IOrdering, ISetting, IStorage } from '../interface';
 
 class Setting {
+  static init() {
+    const defaultSetting: ISetting = {
+      collectionsOrdering: {
+        type: 0,
+        descending: false,
+      },
+      quickSearch: false,
+      darkMode: false,
+    };
+    return defaultSetting;
+  }
+
   static async fetch(): Promise<ISetting> {
-    let localStorage = await Storage.fetch();
-    let setting = localStorage.setting;
+    //Default Data for Setting
+    let setting: ISetting = this.init();
 
-    if (setting == null) {
-      let defaultSetting: ISetting = {
-        collectionsOrdering: {
-          type: 0,
-          descending: false,
-        },
-        quickSearch: false,
-        darkMode: false,
-      };
+    try {
+      //It returns {setting}, so IStorage is better choice, not ISetting
+      let localStorage = (await Browser.storage.local.get(
+        'setting'
+      )) as IStorage;
 
-      return defaultSetting;
+      if (localStorage.setting != null) {
+        setting = localStorage.setting;
+      }
+    } catch (e) {
+      console.error(e);
     }
-
     return setting;
   }
 
-  static async update(_setting: ISetting) {
-    let localStorage = await Storage.fetch();
-    localStorage.setting = _setting;
-    Browser.storage.local.set(localStorage);
+  static async fetchOrdering(): Promise<IOrdering> {
+    let setting = await this.fetch();
+    return setting.collectionsOrdering;
+  }
+
+  static async fetchDarkMode(): Promise<Boolean> {
+    let setting = await this.fetch();
+    return setting.darkMode;
+  }
+
+  static async update(_setting: ISetting): Promise<Boolean> {
+    const setting = _setting;
+    try {
+      await Browser.storage.local.set({ setting });
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+    return true;
+  }
+
+  static async updateOrdering(_ordering: IOrdering): Promise<Boolean> {
+    try {
+      let setting = await this.init();
+      setting.collectionsOrdering = _ordering;
+      this.update(setting);
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+    return true;
+  }
+
+  static async updateDarkMode(_darkMode: boolean): Promise<Boolean> {
+    try {
+      let setting = await this.init();
+      setting.darkMode = _darkMode;
+      this.update(setting);
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+    return true;
   }
 }
 
