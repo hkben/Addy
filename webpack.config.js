c761c6c7d80fc1f4bde6e5503a3d77e554a6b2ba
hkben <liu411hk@gmail.com>
2022-04-04 23:58:30 +0800

    Manifest v3 for Chrome

--- webpack.config.js
+++ webpack.config.js
@@ -133,6 +133,21 @@ var options = {
             );
           },
         },
+        {
+          from: 'src/manifest_v3.json',
+          to: path.join(__dirname, 'build'),
+          force: true,
+          transform: function (content, path) {
+            // generates the manifest file using the package.json informations
+            return Buffer.from(
+              JSON.stringify({
+                description: process.env.npm_package_description,
+                version: process.env.npm_package_version,
+                ...JSON.parse(content.toString()),
+              })
+            );
+          },
+        },
       ],
     }),
     new CopyWebpackPlugin({
