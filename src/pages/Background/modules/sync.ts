1a679fd5bc92d3b6027f9aebcca918b209dd9b68
hkben <liu411hk@gmail.com>
2022-04-27 19:07:01 +0800

    Improve syncing flow con.

--- src/pages/Background/modules/sync.ts
+++ src/pages/Background/modules/sync.ts
@@ -40,35 +40,45 @@ export const syncBackgroundRun = async () => {
   }
 
   //Sync Logic here
-  let init = await syncProvider.init();
-  let fileInfo = await syncProvider.searchSyncFile();
 
-  //If file is not exists on server, create one
-  if (fileInfo == null) {
-    console.log('[Sync] Remote Sync File is not exists, creating...');
-    await syncProvider.createSyncFile();
-  } else {
-    console.log('[Sync] Download Data...');
-    let json = await syncProvider.getSyncFile(fileInfo);
+  let _result = false;
 
-    console.log('[Sync] Importing Data...');
-    const collections: ICollection[] = JSON.parse(json);
+  try {
+    await syncProvider.init();
 
-    if (collections.length > 0) {
-      await Collections.import(collections);
+    let fileInfo = await syncProvider.searchSyncFile();
+
+    //If file is not exists on server, create one
+    if (fileInfo == null) {
+      console.log('[Sync] Remote Sync File is not exists, creating...');
+      await syncProvider.createSyncFile();
+    } else {
+      console.log('[Sync] Download Data...');
+      let json = await syncProvider.getSyncFile(fileInfo);
+
+      console.log('[Sync] Importing Data...');
+      const collections: ICollection[] = JSON.parse(json);
+
+      if (collections.length > 0) {
+        await Collections.import(collections);
+      }
+
+      console.log('[Sync] Uploading imported Data...');
+      await syncProvider.createSyncFile();
     }
 
-    console.log('[Sync] Uploading imported Data...');
-    await syncProvider.createSyncFile();
+    let _datetime = new Date().toISOString();
+    await SyncSetting.updateLastSyncTime(_datetime);
+
+    console.log('[Sync] Done...');
+    _result = true;
+  } catch (error) {
+    console.log('[Sync] Error...');
+    console.error(error);
+  } finally {
+    Browser.runtime.sendMessage({
+      action: 'syncCompleted',
+      result: _result,
+    } as IBrowserMessage);
   }
-
-  let _datetime = new Date().toISOString();
-  await SyncSetting.updateLastSyncTime(_datetime);
-
-  console.log('[Sync] Done...');
-
-  Browser.runtime.sendMessage({
-    action: 'syncCompleted',
-    result: true,
-  } as IBrowserMessage);
 };
