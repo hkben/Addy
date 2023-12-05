01edc784fb5b861b8e942b55c5e1becd4699b27a
hkben <liu411hk@gmail.com>
2023-12-06 02:15:30 +0800

    Fix Settings Ui responsive

--- src/pages/Panel/Component/Settings.tsx
+++ src/pages/Panel/Component/Settings.tsx
@@ -4,8 +4,8 @@ import { Link, Outlet } from 'react-router-dom';
 class Settings extends React.Component<{}> {
   render() {
     return (
-      <div className="container w-full flex flex-wrap mx-auto p-2 m-16">
-        <div className="w-full lg:w-1/5 lg:px-6 text-xl text-gray-800 leading-normal dark:text-gray-50">
+      <div className="xl:container w-full flex flex-wrap mx-auto p-2 m-16">
+        <div className="w-1/5 lg:px-6 text-xl text-gray-800 leading-normal dark:text-gray-50">
           <p className="font-bold underline underline-offset-auto">Settings</p>
 
           <ul className="py-5">
@@ -30,7 +30,7 @@ class Settings extends React.Component<{}> {
           </ul>
         </div>
 
-        <div className="w-full h-full lg:w-4/5 p-8 mt-6 lg:mt-0 text-gray-900 leading-normal bg-white border border-gray-400 dark:text-gray-50 dark:bg-gray-800 dark:border-gray-400">
+        <div className="w-4/5 p-8 text-gray-900 leading-normal bg-white border border-gray-400 dark:text-gray-50 dark:bg-gray-800 dark:border-gray-400">
           <Outlet />
         </div>
       </div>
