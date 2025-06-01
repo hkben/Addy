bed89fac87590b747d0c60fb8f3ab5f85aa6cd75
hkben <liu411hk@gmail.com>
2025-06-02 05:58:05 +0800

    Initialize Setting Store in Panel

--- src/pages/Panel/index.tsx
+++ src/pages/Panel/index.tsx
@@ -4,7 +4,17 @@ import { createRoot } from 'react-dom/client';
 import '@/style.scss';
 import './index.scss';
 import Panel from './Panel';
+import useSettingStore from '@/common/store/useSettingStore';
+import log from 'loglevel';
 
-const container = window.document.querySelector('#app-container');
-const root = createRoot(container!);
-root.render(<Panel />);
+async function initializeApp() {
+  log.debug('[Addy] Initializing...');
+  await useSettingStore.getState().fetch();
+  log.debug('[Addy] Settings fetched:', useSettingStore.getState().setting);
+}
+
+initializeApp().then(() => {
+  const container = window.document.querySelector('#app-container');
+  const root = createRoot(container!);
+  root.render(<Panel />);
+});
