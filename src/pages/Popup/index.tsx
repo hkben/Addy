249c788101fd841b3713687ae8d099dd090a4f51
hkben <liu411hk@gmail.com>
2025-06-07 05:17:41 +0800

    Initialize Setting Store in Popup

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -7,10 +7,20 @@ import '@/style.scss';
 import './index.css';
 import Browser from 'webextension-polyfill';
 import { IBrowserMessage } from '@/common/interface';
+import useSettingStore from '@/common/store/useSettingStore';
+import log from 'loglevel';
 
-const container = window.document.querySelector('#app-container');
-const root = createRoot(container!);
-root.render(<Popup />);
+async function initializeApp() {
+  log.debug('[Addy] Initializing...');
+  await useSettingStore.getState().fetch();
+  log.debug('[Addy] Settings fetched:', useSettingStore.getState().setting);
+}
+
+initializeApp().then(() => {
+  const container = window.document.querySelector('#app-container');
+  const root = createRoot(container!);
+  root.render(<Popup />);
+});
 
 Browser.runtime.onMessage.addListener(onMessageListener);
 
