95de58a41a73285d4f2d20e466895cef7c3a2761
hkben <liu411hk@gmail.com>
2025-08-12 03:28:51 +0800

    Update CollectionViewer layout with header

--- src/pages/Panel/pages/CollectionViewer.tsx
+++ src/pages/Panel/pages/CollectionViewer.tsx
@@ -24,6 +24,8 @@ import useCollectionsListStore from '@/common/hooks/useCollectionsListStore';
 import useCollectionStore from '@/common/hooks/useCollectionStore';
 import log from 'loglevel';
 import useSettingStore from '@/common/store/useSettingStore';
+import { SidebarInset } from '@/components/ui/sidebar';
+import Header from '../layouts/Header';
 
 export async function loader({ params }: LoaderFunctionArgs<any>) {
   var result = await useCollectionStore
@@ -137,163 +139,170 @@ function CollectionViewer() {
   }, [data]);
 
   return (
-    <div>
-      <div className="w-full py-5 flex gap-2.5">
-        <p className="text-3xl my-auto">
-          {editCollectionName ? (
-            <input
-              className="px-2 border-solid border-2 border-grey-600 rounded-lg dark:bg-gray-800"
-              type="text"
-              value={collectionName}
-              onChange={handleCollectionNameChange}
-            />
-          ) : (
-            collectionName
-          )}
-        </p>
+    <SidebarInset>
+      <Header title="Addy" />
+      <div className="flex flex-1 flex-col gap-4 px-4 py-5">
+        <div className="bg-muted/50 mx-auto h-full w-full max-w-5xl rounded-xl p-8">
+          <div className="w-full py-5 flex gap-2.5">
+            <p className="text-3xl my-auto">
+              {editCollectionName ? (
+                <input
+                  className="px-2 border-solid border-2 border-grey-600 rounded-lg dark:bg-gray-800"
+                  type="text"
+                  value={collectionName}
+                  onChange={handleCollectionNameChange}
+                />
+              ) : (
+                collectionName
+              )}
+            </p>
 
-        <button
-          className="h-10 w-10 p-1 px-1.5 text-white bg-blue-500 hover:bg-blue-700 rounded-md items-center"
-          title="Edit Name"
-          onClick={() => {
-            if (editCollectionName == false) {
-              setEditCollectionName(true);
-            } else {
-              handleCollectionNameSubmbit();
-            }
-          }}
-        >
-          <PencilIcon className="h-6 w-6 m-auto" strokeWidth={2} />
-        </button>
+            <button
+              className="h-10 w-10 p-1 px-1.5 text-white bg-blue-500 hover:bg-blue-700 rounded-md items-center"
+              title="Edit Name"
+              onClick={() => {
+                if (editCollectionName == false) {
+                  setEditCollectionName(true);
+                } else {
+                  handleCollectionNameSubmbit();
+                }
+              }}
+            >
+              <PencilIcon className="h-6 w-6 m-auto" strokeWidth={2} />
+            </button>
 
-        <button
-          className="h-10 w-10 p-1 px-1.5 text-white bg-blue-500 hover:bg-blue-700 rounded-md items-center"
-          title="Empty Collection"
-          onClick={() => {
-            const confirmBox = window.confirm(
-              'Do you really want to delete all items in this collection?'
-            );
-            if (confirmBox === true) {
-              removeAllItems(collection.id);
-            }
-          }}
-        >
-          <DocumentMinusIcon className="h-6 w-6 m-auto" strokeWidth={2} />
-        </button>
+            <button
+              className="h-10 w-10 p-1 px-1.5 text-white bg-blue-500 hover:bg-blue-700 rounded-md items-center"
+              title="Empty Collection"
+              onClick={() => {
+                const confirmBox = window.confirm(
+                  'Do you really want to delete all items in this collection?'
+                );
+                if (confirmBox === true) {
+                  removeAllItems(collection.id);
+                }
+              }}
+            >
+              <DocumentMinusIcon className="h-6 w-6 m-auto" strokeWidth={2} />
+            </button>
 
-        <button
-          className="h-10 w-10 p-1 px-1.5 text-white bg-blue-500 hover:bg-blue-700 rounded-md items-center"
-          title="Delete Collection"
-          onClick={() => {
-            const confirmBox = window.confirm(
-              'Do you really want to delete this collection?'
-            );
-            if (confirmBox === true) {
-              removeCollection();
-            }
-          }}
-        >
-          <TrashIcon className="h-6 w-6 m-auto" strokeWidth={2} />
-        </button>
+            <button
+              className="h-10 w-10 p-1 px-1.5 text-white bg-blue-500 hover:bg-blue-700 rounded-md items-center"
+              title="Delete Collection"
+              onClick={() => {
+                const confirmBox = window.confirm(
+                  'Do you really want to delete this collection?'
+                );
+                if (confirmBox === true) {
+                  removeCollection();
+                }
+              }}
+            >
+              <TrashIcon className="h-6 w-6 m-auto" strokeWidth={2} />
+            </button>
 
-        <div
-          className={`${
-            collection.color == 0 || collection.color == null
-              ? ''
-              : 'border-dotted '
-          } rounded-full my-auto h-6 w-6 cursor-pointer bg-transparent border-2 border-gray-700 dark:border-white`}
-          onClick={() => changeCollectionColor(collection.id, 0)}
-        ></div>
-        <div
-          className={`${
-            collection.color == 1 ? 'active-color' : ''
-          } rounded-full my-auto h-6 w-6 cursor-pointer color-1`}
-          onClick={() => changeCollectionColor(collection.id, 1)}
-        ></div>
-        <div
-          className={`${
-            collection.color == 2 ? 'active-color' : ''
-          } rounded-full my-auto h-6 w-6 cursor-pointer color-2`}
-          onClick={() => changeCollectionColor(collection.id, 2)}
-        ></div>
-        <div
-          className={`${
-            collection.color == 3 ? 'active-color' : ''
-          } rounded-full my-auto h-6 w-6 cursor-pointer color-3`}
-          onClick={() => changeCollectionColor(collection.id, 3)}
-        ></div>
-        <div
-          className={`${
-            collection.color == 4 ? 'active-color' : ''
-          } rounded-full my-auto h-6 w-6 cursor-pointer color-4`}
-          onClick={() => changeCollectionColor(collection.id, 4)}
-        ></div>
-        <div
-          className={`${
-            collection.color == 5 ? 'active-color' : ''
-          } rounded-full my-auto h-6 w-6 cursor-pointer color-5`}
-          onClick={() => changeCollectionColor(collection.id, 5)}
-        ></div>
-        <div
-          className={`${
-            collection.color == 6 ? 'active-color' : ''
-          } rounded-full my-auto h-6 w-6 cursor-pointer color-6`}
-          onClick={() => changeCollectionColor(collection.id, 6)}
-        ></div>
+            <div
+              className={`${
+                collection.color == 0 || collection.color == null
+                  ? ''
+                  : 'border-dotted '
+              } rounded-full my-auto h-6 w-6 cursor-pointer bg-transparent border-2 border-gray-700 dark:border-white`}
+              onClick={() => changeCollectionColor(collection.id, 0)}
+            ></div>
+            <div
+              className={`${
+                collection.color == 1 ? 'active-color' : ''
+              } rounded-full my-auto h-6 w-6 cursor-pointer color-1`}
+              onClick={() => changeCollectionColor(collection.id, 1)}
+            ></div>
+            <div
+              className={`${
+                collection.color == 2 ? 'active-color' : ''
+              } rounded-full my-auto h-6 w-6 cursor-pointer color-2`}
+              onClick={() => changeCollectionColor(collection.id, 2)}
+            ></div>
+            <div
+              className={`${
+                collection.color == 3 ? 'active-color' : ''
+              } rounded-full my-auto h-6 w-6 cursor-pointer color-3`}
+              onClick={() => changeCollectionColor(collection.id, 3)}
+            ></div>
+            <div
+              className={`${
+                collection.color == 4 ? 'active-color' : ''
+              } rounded-full my-auto h-6 w-6 cursor-pointer color-4`}
+              onClick={() => changeCollectionColor(collection.id, 4)}
+            ></div>
+            <div
+              className={`${
+                collection.color == 5 ? 'active-color' : ''
+              } rounded-full my-auto h-6 w-6 cursor-pointer color-5`}
+              onClick={() => changeCollectionColor(collection.id, 5)}
+            ></div>
+            <div
+              className={`${
+                collection.color == 6 ? 'active-color' : ''
+              } rounded-full my-auto h-6 w-6 cursor-pointer color-6`}
+              onClick={() => changeCollectionColor(collection.id, 6)}
+            ></div>
+          </div>
+
+          <div className="w-2/3 items-center text-xs rounded-md">
+            <button
+              className={`${
+                collectionType == 0
+                  ? 'bg-gray-200 dark:bg-gray-600'
+                  : 'bg-white dark:bg-gray-800'
+              } w-1/4 px-5 py-3 font-semibold border rounded-l-lg border-gray-300`}
+              type="button"
+              onClick={() => changeType(0)}
+            >
+              📕 All ({itemCount.all})
+            </button>
+
+            <button
+              className={`${
+                collectionType == 1
+                  ? 'bg-gray-200 dark:bg-gray-600'
+                  : 'bg-white dark:bg-gray-800'
+              } w-1/4 px-5 py-3 font-semibold border-y border-r border-gray-300`}
+              type="button"
+              onClick={() => changeType(1)}
+            >
+              📝 Text ({itemCount.text})
+            </button>
+
+            <button
+              className={`${
+                collectionType == 2
+                  ? 'bg-gray-200 dark:bg-gray-600'
+                  : 'bg-white dark:bg-gray-800'
+              } w-1/4 px-5 py-3 font-semibold border-y border-r border-gray-300`}
+              type="button"
+              onClick={() => changeType(2)}
+            >
+              🖼️ Image ({itemCount.image})
+            </button>
+
+            <button
+              className={`${
+                collectionType == 3
+                  ? 'bg-gray-200 dark:bg-gray-600'
+                  : 'bg-white dark:bg-gray-800'
+              } w-1/4 px-5 py-3 font-semibold border-r border-y rounded-r-lg border-gray-300`}
+              type="button"
+              onClick={() => changeType(3)}
+            >
+              🔖 Bookmark ({itemCount.bookmark})
+            </button>
+          </div>
+
+          <div className="py-8">
+            {collectionType == 2 ? imageViewer : viewer}
+          </div>
+        </div>
       </div>
-
-      <div className="w-2/3 items-center text-xs rounded-md">
-        <button
-          className={`${
-            collectionType == 0
-              ? 'bg-gray-200 dark:bg-gray-600'
-              : 'bg-white dark:bg-gray-800'
-          } w-1/4 px-5 py-3 font-semibold border rounded-l-lg border-gray-300`}
-          type="button"
-          onClick={() => changeType(0)}
-        >
-          📕 All ({itemCount.all})
-        </button>
-
-        <button
-          className={`${
-            collectionType == 1
-              ? 'bg-gray-200 dark:bg-gray-600'
-              : 'bg-white dark:bg-gray-800'
-          } w-1/4 px-5 py-3 font-semibold border-y border-r border-gray-300`}
-          type="button"
-          onClick={() => changeType(1)}
-        >
-          📝 Text ({itemCount.text})
-        </button>
-
-        <button
-          className={`${
-            collectionType == 2
-              ? 'bg-gray-200 dark:bg-gray-600'
-              : 'bg-white dark:bg-gray-800'
-          } w-1/4 px-5 py-3 font-semibold border-y border-r border-gray-300`}
-          type="button"
-          onClick={() => changeType(2)}
-        >
-          🖼️ Image ({itemCount.image})
-        </button>
-
-        <button
-          className={`${
-            collectionType == 3
-              ? 'bg-gray-200 dark:bg-gray-600'
-              : 'bg-white dark:bg-gray-800'
-          } w-1/4 px-5 py-3 font-semibold border-r border-y rounded-r-lg border-gray-300`}
-          type="button"
-          onClick={() => changeType(3)}
-        >
-          🔖 Bookmark ({itemCount.bookmark})
-        </button>
-      </div>
-
-      <div className="py-8">{collectionType == 2 ? imageViewer : viewer}</div>
-    </div>
+    </SidebarInset>
   );
 }
 
