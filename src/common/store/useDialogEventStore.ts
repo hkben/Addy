cfb7c374b50cb67f5b397082dd72c30600cf1440
hkben <liu411hk@gmail.com>
2025-08-16 23:18:01 +0800

    Add useDialogEventStore for dialog event state

---
+++ src/common/store/useDialogEventStore.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+import log from 'loglevel';
+
+enum DialogEventType {
+  Delete,
+  Edit,
+}
+
+interface DialogEvent {
+  type: DialogEventType;
+  collectionId?: string;
+  itemId?: string;
+}
+
+interface Store {
+  event?: DialogEvent;
+  setDialogEvent: (event: DialogEvent) => void;
+  resetDialogEvent: () => void;
+}
+
+const useDialogEventStore = create<Store>()(
+  immer((set, get) => ({
+    // Initial state
+    event: undefined,
+    setDialogEvent: (event: DialogEvent) => {
+      log.debug('[useDialogEventStore] setDialogEvent', event);
+      set({ event });
+    },
+    resetDialogEvent: () => {
+      log.debug('[useDialogEventStore] resetDialogEvent');
+      set({ event: undefined });
+    },
+  }))
+);
+
+export { DialogEventType, useDialogEventStore };
