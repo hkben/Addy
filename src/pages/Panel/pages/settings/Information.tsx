9ce1a7cbb83b95ad5388ebde2c214d15aca481b0
hkben <liu411hk@gmail.com>
2025-08-26 03:41:41 +0800

    Update Information page Ui

--- src/pages/Panel/pages/settings/Information.tsx
+++ src/pages/Panel/pages/settings/Information.tsx
@@ -6,7 +6,10 @@ function Information() {
 
   return (
     <div>
-      <p className="text-3xl py-2">Information</p>
+      <div className="grid gap-1 mb-4">
+        <p className="text-3xl font-bold py-2">Information</p>
+      </div>
+
       <div className="w-full border-t divide-y">
         <div className="py-4 w-3/4">
           <img className="h-20 w-20" src="icon-128.png" />
@@ -19,25 +22,42 @@ function Information() {
           <div className="py-6 text-base">
             <ul className="list-disc list-inside">
               <li>
-                <a href="https://www.buymeacoffee.com/hkben">
+                <a
+                  className="font-medium underline underline-offset-4"
+                  href="https://www.buymeacoffee.com/hkben"
+                >
                   Buy Me a Coffee!
                 </a>
               </li>
               <li>
-                <a href="https://github.com/hkben/Addy">Github</a>
+                <a
+                  className="font-medium underline underline-offset-4"
+                  href="https://github.com/hkben/Addy"
+                >
+                  Github
+                </a>
               </li>
               <li>
-                <a href="https://addons.mozilla.org/firefox/addon/addy/">
+                <a
+                  className="font-medium underline underline-offset-4"
+                  href="https://addons.mozilla.org/firefox/addon/addy/"
+                >
                   Firefox Add-ons
                 </a>
               </li>
               <li>
-                <a href="https://chrome.google.com/webstore/detail/addy/aeopkajhbaodnaelhbblokdngiapbglk">
+                <a
+                  className="font-medium underline underline-offset-4"
+                  href="https://chrome.google.com/webstore/detail/addy/aeopkajhbaodnaelhbblokdngiapbglk"
+                >
                   Chrome Web Store
                 </a>
               </li>
               <li>
-                <a href="https://addy.insanelyones.com/privacy-policy">
+                <a
+                  className="font-medium underline underline-offset-4"
+                  href="https://addy.insanelyones.com/privacy-policy"
+                >
                   Privacy Policy
                 </a>
               </li>
